802702020b370bd3a5b50bb84a1ffda75b38db40
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-05-23 20:19:44 -0700

    movedserverfile

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
