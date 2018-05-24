9fa0e1a24ce41236ddf464a3bcc45a793d9eeb03
Bicen Ye <bicenye@Bicens-MacBook-Pro.local>
2018-05-23 20:24:27 -0700

    updatedpath

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyParser.json());
 
 
 // ROUTER
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 // Starts the server to begin listening
